2687bba8a6208814d10c1d848f4549b71ccecfe5
malavikasmenon <42709307+malavikasmenon@users.noreply.github.com>
2018-10-20 16:05:50 +0530

    Update god_why.cpp

--- god_why.cpp
+++ god_why.cpp
@@ -1,38 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-using std::string;
- 
+/*using std::string;
+
 static struct IO {
 	char tmp[1 << 10];
- 
+
 	// fast input routines
 	char cur;
- 
+
 //#define nextChar() (cur = getc_unlocked(stdin))
 //#define peekChar() (cur)
 	inline char nextChar() { return cur = getc_unlocked(stdin); }
 	inline char peekChar() { return cur; }
- 
+
 	inline operator bool() { return peekChar(); }
 	inline static bool isBlank(char c) { return (c < '-' && c); }
 	inline bool skipBlanks() { while (isBlank(nextChar())); return peekChar() != 0; }
- 
+
 	inline IO& operator >> (char & c) { c = nextChar(); return *this; }
- 
+
 	inline IO& operator >> (char * buf) {
 		if (skipBlanks()) {
 			if (peekChar()) {
 				*(buf++) = peekChar();
 				while (!isBlank(nextChar())) *(buf++) = peekChar();
 			} *(buf++) = 0; } return *this; }
- 
+
 	inline IO& operator >> (string & s) {
 		if (skipBlanks()) {	s.clear(); s += peekChar();
 			while (!isBlank(nextChar())) s += peekChar(); }
 		return *this; }
- 
+
 	inline IO& operator >> (double & d) { if ((*this) >> tmp) sscanf(tmp, "%lf", &d); return *this;	}
- 
+
 #define defineInFor(intType) \
 	inline IO& operator >>(intType & n) { \
 		if (skipBlanks()) { \
@@ -49,24 +49,24 @@ static struct IO {
 		} \
 		return *this; \
 	}
- 
+
 defineInFor(int)
 defineInFor(unsigned int)
 defineInFor(long long)
- 
+
 	// fast output routines
- 
+
 //#define putChar(c) putc_unlocked((c), stdout)
 	inline void putChar(char c) { putc_unlocked(c, stdout); }
 	inline IO& operator << (char c) { putChar(c); return *this; }
 	inline IO& operator << (const char * s) { while (*s) putChar(*s++); return *this; }
- 
+
 	inline IO& operator << (const string & s) { for (int i = 0; i < (int)s.size(); ++i) putChar(s[i]); return *this; }
- 
+
 	char * toString(double d) { sprintf(tmp, "%lf%c", d, '\0'); return tmp; }
 	inline IO& operator << (double d) { return (*this) << toString(d); }
- 
- 
+
+
 #define defineOutFor(intType) \
 	inline char * toString(intType n) { \
 		char * p = (tmp + 30); \
@@ -80,20 +80,21 @@ defineInFor(long long)
 		return p; \
 	} \
 	inline IO& operator << (intType n) { return (*this) << toString(n); }
- 
+
 defineOutFor(int)
 defineOutFor(long long)
- 
+
 #define endl ('\n')
 #define cout __io__
 #define cin __io__
 } __io__;
-
+*/
 int main(int argc, char const *argv[]){
 	register int t;
 	cin>>t;
 	while(t--){
 		register int a,b,temp;
+		cin>>a>>b;
 		register bool flag=true;
 		vector<int> v;
 		for(register int i=0;i<b;i++){
